766613894319fa48b843269f0863e8143d4d91c4
Aleksi Backman <arbiter0xf@protonmail.com>
2024-11-02 21:15:09 +0200

    Add second xml test

--- test/TestXml.cpp
+++ test/TestXml.cpp
@@ -22,3 +22,19 @@ TEST_F(TestXml, ParseXmlFromFile)
 
     EXPECT_TRUE(result);
 }
+
+TEST_F(TestXml, ReadTilesetGridWidth)
+{
+    int width;
+
+    pugi::xml_document document;
+
+    width = -1;
+
+    (void) document.load_file("test_data/dc-dngn-collection-spritesheet.tsx");
+
+    pugi::xml_node grid = document.child("tileset").child("grid");
+    width = grid.attribute("width").as_int();
+
+    EXPECT_EQ(width, 1);
+}
