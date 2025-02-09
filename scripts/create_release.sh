b718637f6bfe20e21c2b13846f5310b5f64fedb8
Aleksi Backman <bddubbb@protonmail.com>
2025-02-09 16:08:00 +0200

    Add beginning of release package creation
    Install build dependencies and run unit tests before creating
    release packages and uploading them.

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+set -e
+
+cd $(dirname $0)
+
+readonly ROGUE_FOREVER_BASE_PATH="$(pushd .. &> /dev/null; pwd ; popd &> /dev/null)"
+
 error_exit() {
     echo "${1}"
     exit 1
+}
+
+create_release_packages() {
+    pushd ${ROGUE_FOREVER_BASE_PATH}
+
+    ./scripts/install_build_dependencies.sh
+
+    make test
+
+    # TODO create Linux release package
+
+    # TODO create Windows release package
+
+    popd # ${ROGUE_FOREVER_BASE_PATH}
 }
 
 main() {
@@ -12,6 +32,12 @@ main() {
 
     echo -e "ABTEST RELEASE_TAG is: ${RELEASE_TAG}"
 
+    create_release_packages
+
+    echo "ABTEST exiting early. Testing release package creation."
+    exit 1
+
+    # Post new release to GitHub
     curl -L \
         -X POST \
         -H "Accept: application/vnd.github+json" \
@@ -19,6 +45,8 @@ main() {
         -H "X-GitHub-Api-Version: 2022-11-28" \
         https://api.github.com/repos/pottumuusi/rogue-forever/releases \
         -d "{\"tag_name\":\"${RELEASE_TAG}\"}"
+
+    # TODO Upload release packages
 }
 
 main "${@}"
