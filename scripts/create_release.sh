0f5d755bf67dfa8931449f48a39a04a64f3d644c
Aleksi Backman <bddubbb@protonmail.com>
2025-02-17 16:41:42 +0200

    Organize code using functions

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -72,11 +72,48 @@ create_release_packages() {
     create_release_package_windows
 }
 
+post_new_release_to_github() {
+    output_curl="$(curl -L \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${GH_TOKEN}" \
+        -H "X-GitHub-Api-Version: ${github_api_version}" \
+        https://api.github.com/repos/pottumuusi/rogue-forever/releases \
+        -d "{\"tag_name\":\"${RELEASE_TAG}\"}")"
+
+    rogue_forever_upload_url="$(echo ${output_curl} \
+        | jq '.upload_url' \
+        | cut -d '{' -f 1 \
+        | tr -d \")"
+}
+
+post_release_asset_to_github() {
+    local uploaded_asset_name=''
+
+    local release_asset="${1}"
+
+    output_curl="$(curl -L \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${GH_TOKEN}" \
+        -H "X-GitHub-Api-Version: ${github_api_version}" \
+        -H "Content-Type: application/octet-stream" \
+        "${rogue_forever_upload_url}?name=${release_asset}" \
+        --data-binary "@${release_asset}")"
+
+    uploaded_asset_name=$(echo ${output_curl} \
+        | jq '.name' \
+        | tr -d \")
+
+    if [ "${uploaded_asset_name}" != "${release_asset}" ] ; then
+        error_exit "Release asset name ${release_asset} not detected from response."
+    fi
+}
+
 main() {
     local -r github_api_version="2022-11-28"
 
     local output_curl=''
-    local uploaded_asset_name=''
     local rogue_forever_upload_url=''
 
     if [ -z "${GH_TOKEN}" ] ; then
@@ -91,52 +128,13 @@ main() {
 
     create_release_packages
 
-    # Post new release to GitHub
-    output_curl="$(curl -L \
-        -X POST \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${GH_TOKEN}" \
-        -H "X-GitHub-Api-Version: ${github_api_version}" \
-        https://api.github.com/repos/pottumuusi/rogue-forever/releases \
-        -d "{\"tag_name\":\"${RELEASE_TAG}\"}")"
-    rogue_forever_upload_url="$(echo ${output_curl} \
-        | jq '.upload_url' \
-        | cut -d '{' -f 1 \
-        | tr -d \")"
+    post_new_release_to_github
     if [ "null" == "${rogue_forever_upload_url}" ] ; then
         error_exit "Assets URL is null"
     fi
 
-    # Post release assets to GitHub
-    output_curl="$(curl -L \
-        -X POST \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${GH_TOKEN}" \
-        -H "X-GitHub-Api-Version: ${github_api_version}" \
-        -H "Content-Type: application/octet-stream" \
-        "${rogue_forever_upload_url}?name=${RELEASE_ZIP_LINUX}" \
-        --data-binary "@${RELEASE_ZIP_LINUX}")"
-    uploaded_asset_name=$(echo ${output_curl} \
-        | jq '.name' \
-        | tr -d \")
-    if [ "${uploaded_asset_name}" != "${RELEASE_ZIP_LINUX}" ] ; then
-        error_exit "Linux release zip name not detected from response."
-    fi
-
-    output_curl="$(curl -L \
-        -X POST \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${GH_TOKEN}" \
-        -H "X-GitHub-Api-Version: ${github_api_version}" \
-        -H "Content-Type: application/octet-stream" \
-        "${rogue_forever_upload_url}?name=${RELEASE_ZIP_WINDOWS}" \
-        --data-binary "@${RELEASE_ZIP_WINDOWS}")"
-    uploaded_asset_name=$(echo ${output_curl} \
-        | jq '.name' \
-        | tr -d \")
-    if [ "${uploaded_asset_name}" != "${RELEASE_ZIP_WINDOWS}" ] ; then
-        error_exit "Windows release zip name not detected from response."
-    fi
+    post_release_asset_to_github ${RELEASE_ZIP_LINUX}
+    post_release_asset_to_github ${RELEASE_ZIP_WINDOWS}
 
     popd # ${ROGUE_FOREVER_BASE_PATH}
 }
