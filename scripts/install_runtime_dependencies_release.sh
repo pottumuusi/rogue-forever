bb673a67788de312df82600650335a6809da2d57
Aleksi Backman <bddubbb@protonmail.com>
2025-03-01 22:44:48 +0200

    Remove libsdl package install from runtime dependency install script
    In addition to SDL (possibly from libsdl package), SDL_image is
    needed. There is Debian package libsdl-image1.2, but no any
    libsdl-image2.
    
    It is anyway a scetchy idea to build with one version of libraries
    and run with another version of libraries. Proceeding to recommend
    building SDL and SDL_image from source when installing them as
    runtime dependencies.

--- scripts/install_runtime_dependencies_release.sh
+++ scripts/install_runtime_dependencies_release.sh
@@ -4,19 +4,8 @@ set -e
 
 readonly INSTALL_ASSETS="TRUE"
 readonly INSTALL_MAPS="TRUE"
-readonly INSTALL_LIBSDL2="TRUE"
 
 cd $(dirname $0)
-
-install_libsdl2_debian() {
-    echo "Installing libsdl2-2.0-0"
-    sudo apt update
-    sudo apt install libsdl2-2.0-0
-}
-
-install_libsdl2() {
-    install_libsdl2_debian
-}
 
 main() {
     if [ "TRUE" == "${INSTALL_ASSETS}" ] ; then
@@ -29,10 +18,6 @@ main() {
         echo "Installing maps"
         wget http://gitlab.justworks.today/rogue-forever/maps_basic.zip
         unzip maps_basic.zip
-    fi
-
-    if [ "TRUE" == "${INSTALL_LIBSDL2}" ] ; then
-        install_libsdl2
     fi
 }
 
