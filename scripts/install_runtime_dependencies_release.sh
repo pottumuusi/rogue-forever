4ad8ea471a547ba8bee74862e9477ea23c074d55
Aleksi Backman <arbiter0xf@protonmail.com>
2025-03-02 18:20:25 +0200

    Cleanup zip files after extraction

--- scripts/install_runtime_dependencies_release.sh
+++ scripts/install_runtime_dependencies_release.sh
@@ -16,12 +16,14 @@ main() {
         echo "Installing assets"
         wget http://gitlab.justworks.today/rogue-forever/assets_basic.zip
         unzip assets_basic.zip
+        rm --verbose assets_basic.zip
     fi
 
     if [ "TRUE" == "${INSTALL_MAPS}" ] ; then
         echo "Installing maps"
         wget http://gitlab.justworks.today/rogue-forever/maps_basic.zip
         unzip maps_basic.zip
+        rm --verbose maps_basic.zip
     fi
 
     popd # ${ROGUE_FOREVER_BASE_PATH}
